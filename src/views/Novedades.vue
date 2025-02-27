5c0e09ff3691e09fd3e3528dd0e415113fa469d0
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-02-27 19:19:58 +0100

    arreglo novedades

--- src/views/Novedades.vue
+++ src/views/Novedades.vue
@@ -24,12 +24,12 @@
   import { ref } from 'vue';
   
   const novedades = ref([
-    { nombre: 'Producto 1', imagen: '/images/producto1.jpg', descripcion: 'Descripción del producto 1' },
-    { nombre: 'Producto 2', imagen: '/images/producto2.jpg', descripcion: 'Descripción del producto 2' },
-    { nombre: 'Producto 3', imagen: '/images/producto3.jpg', descripcion: 'Descripción del producto 3' },
-    { nombre: 'Producto 4', imagen: '/images/producto4.jpg', descripcion: 'Descripción del producto 4' },
-    { nombre: 'Producto 5', imagen: '/images/producto5.jpg', descripcion: 'Descripción del producto 5' },
-    { nombre: 'Producto 6', imagen: '/images/producto6.jpg', descripcion: 'Descripción del producto 6' }
+    { nombre: 'Producto 1', imagen: 'https://plus.unsplash.com/premium_photo-1672163163579-e5d4aedd26af?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVkaWNpbmFzfGVufDB8fDB8fHww', descripcion: 'Descripción del producto 1' },
+    { nombre: 'Producto 2', imagen: 'https://www.shutterstock.com/image-photo/variety-medicines-drugsmedicine-healthcare-concept-260nw-2505906047.jpg', descripcion: 'Descripción del producto 2' },
+    { nombre: 'Producto 3', imagen: 'https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/177B/production/_97211060_capture.jpg.webp', descripcion: 'Descripción del producto 3' },
+    { nombre: 'Producto 4', imagen: 'https://www.natruly.com/cdn/shop/collections/cremas.jpg?v=1712256467&width=3200', descripcion: 'Descripción del producto 4' },
+    { nombre: 'Producto 5', imagen: 'https://www.samparfums.es/blog/wp-content/uploads/2023/12/crema-aceite-locion-corporal.webp', descripcion: 'Descripción del producto 5' },
+    { nombre: 'Producto 6', imagen: 'https://info.mercadona.es/img-cont/es/cremas-de-manos-deliplus--mov.jpg', descripcion: 'Descripción del producto 6' }
   ]);
   </script>
   
