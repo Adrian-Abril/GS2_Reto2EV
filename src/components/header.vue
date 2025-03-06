7aa82b9774c63d728a1e646e982fb4c4635cf887
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-03-06 16:35:05 +0100

    menu hamburgesa header

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,22 +3,33 @@
     <div class="container">
       <div class="logo">
         <router-link to="/" class="logo__link">
-          <img src="/src/assets/images/1.png" alt="VOLUNMED Logo" class="logo__image" />
+          <img src="/src/assets/images/1.png" alt="FARMED Logo" class="logo__image" />
           <div>
             <h1>FARMED</h1>
             <span>Somos farmacéuticos</span>
           </div>
         </router-link>
       </div>
-      <nav class="nav">
+
+      <!-- Botón hamburguesa -->
+      <button class="menu-toggle" @click="toggleMenu" :class="{ open: isMenuOpen }">
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
+
+      <!-- Menú de navegación -->
+      <nav class="nav" :class="{ 'nav--open': isMenuOpen }">
         <ul>
-          <li><router-link to="/">Categorías</router-link></li>
-          <li><router-link to="/medicamentos">Medicamentos</router-link></li>
-          <li><router-link to="/novedades">Novedades</router-link></li>
-          <li><router-link to="/oportunidades">Oportunidades</router-link></li>
-          <li><router-link to="/blog">Blog</router-link></li>
+          <li><router-link to="/" @click="closeMenu">Categorías</router-link></li>
+          <li><router-link to="/medicamentos" @click="closeMenu">Medicamentos</router-link></li>
+          <li><router-link to="/novedades" @click="closeMenu">Novedades</router-link></li>
+          <li><router-link to="/oportunidades" @click="closeMenu">Oportunidades</router-link></li>
+          <li><router-link to="/blog" @click="closeMenu">Blog</router-link></li>
         </ul>
       </nav>
+
+      <!-- Acciones (login y carrito) -->
       <div class="actions">
         <router-link to="/login" class="login">Iniciar sesión</router-link>
         <button class="cart"></button>
@@ -28,6 +39,17 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+
+const closeMenu = () => {
+  isMenuOpen.value = false;
+};
 </script>
 
 <style scoped lang="scss">
