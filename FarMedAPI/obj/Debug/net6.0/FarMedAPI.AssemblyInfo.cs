056e0b6890c058e665efcf655955f7aa06929b48
a24977 <a24977@svalero.com>
2025-03-15 19:29:38 +0100

    re

--- FarMedAPI/obj/Debug/net6.0/FarMedAPI.AssemblyInfo.cs
+++ FarMedAPI/obj/Debug/net6.0/FarMedAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FarMedAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fdc5176facfd3734c8c3982304a02591d9eb047")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a2a3a32243daffe78445c964dd3222d60385f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FarMedAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FarMedAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
